286e001384a68c682ab0aa9a99f257ef3714ba64
JWsoundsystems <JWsoundsystems@gmail.com>
2019-11-10 23:56:48 -0800

    Successful response from BART API for entire Train Schedule Array in Console Log on click

--- assets/java.js
+++ assets/java.js
@@ -1,16 +1,18 @@
 var api = "http://api.bart.gov/api/etd.aspx";
-var query = "?cmd=etd&orig=ANTC";
+var query = "?cmd=etd&orig=";
 var key = "&key=MW9S-E7SL-26DU-VV8V&json=y";
-
-let array = []
+let array = ['12TH', '16TH', '19TH', '24TH', 'ANTC', 'ASHB', 'BALB', 'BAYF', 'CAST', 'CIVC', 'COLS', 'COLM', 'CONC', 'DALY', 'DBRK', 'DUBL', 'DELN', 'PLZA', 'EMBR', 'FRMT', 'FTVL', 'GLEN', 'HAYW', 'LAFY', 'LAKE', 'MCAR', 'MLBR', 'MONT', 'NBRK', 'NCON', 'OAKL', 'ORIN', 'PITT', 'PCTR', 'PHIL', 'POWL', 'RICH', 'ROCK', 'SBRN', 'SFIA', 'SANL', 'SHAY', 'SSAN', 'UCTY', 'WCRK', 'WARM', 'WDUB', 'WOAK']
 
 
 $("#search").on('click', function() {
     console.log("Button Clicked!") 
-    var xhr = $.get(api + query + key).done(function (response) {
+    for (let i = 0; i < array.length; i++) {
+    var station = `${array[i]}`
+    var xhr = $.get(api + query + station + key).done(function (response) {
         console.log("success got data", xhr, response);
+    }) 
+          }
     
-      }) 
 }); 
 
 
