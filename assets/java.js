2fb6b5222c2a754e365b89b4e0dfd2803aabc3a4
JWsoundsystems <JWsoundsystems@gmail.com>
2019-11-11 20:59:51 -0800

    successfully appending train data to page

--- assets/java.js
+++ assets/java.js
@@ -4,16 +4,25 @@ var key = "&key=MW9S-E7SL-26DU-VV8V&json=y";
 let array = ['12TH', '16TH', '19TH', '24TH', 'ANTC', 'ASHB', 'BALB', 'BAYF', 'CAST', 'CIVC', 'COLS', 'COLM', 'CONC', 'DALY', 'DBRK', 'DUBL', 'DELN', 'PLZA', 'EMBR', 'FRMT', 'FTVL', 'GLEN', 'HAYW', 'LAFY', 'LAKE', 'MCAR', 'MLBR', 'MONT', 'NBRK', 'NCON', 'OAKL', 'ORIN', 'PITT', 'PCTR', 'PHIL', 'POWL', 'RICH', 'ROCK', 'SBRN', 'SFIA', 'SANL', 'SHAY', 'SSAN', 'UCTY', 'WCRK', 'WARM', 'WDUB', 'WOAK']
 
 
-$("#search").on('click', function() {
-    console.log("Button Clicked!") 
+$("#search").on('click', function () {
+    console.log("Button Clicked!")
     for (let i = 0; i < array.length; i++) {
-    var station = `${array[i]}`
-    var xhr = $.get(api + query + station + key).done(function (response) {
-        console.log("success got data", xhr, response);
-    }) 
-          }
-    
-}); 
+        var station = `${array[i]}`
+        var xhr = $.get(api + query + station + key).done(function (responseJSON) {
+            // console.log("success got data");
 
+            var bartReply = responseJSON
+            console.log(bartReply.root);
 
-  
+            for (i in bartReply) {
+                $("#1").append(`${bartReply.root.station[0].name}`)
+                $("#2").append(`${bartReply.root.station[0].etd[0].destination}`)
+                $("#3").append(`${bartReply.root.station[0].etd[0].estimate[0].direction}`)
+                $("#4").append(`${bartReply.root.station[0].etd[0].estimate[0].minutes}`)
+                // $("#5").append(`${bartReply.root.station[0].etd[0].destination}`)
+          
+              }
+
+        })
+    }
+});
